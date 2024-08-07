8646dcb55198dc8e18fbd9590f289fdbe6e220cf
ProfeChrisJara <cjjarav@gmail.com>
2024-08-06 23:47:24 -0500

    correcciones

--- test_3.py
+++ test_3.py
@@ -3,9 +3,9 @@ from io import StringIO
 import sys
 
 def test_imprimir_datos_personales(capsys):
-    nombre = "Christopher Jara"
-    edad = 38
-    estatura = 1.73
+    nombre = "Tu nombre"
+    edad = 30
+    estatura = 1.75
     imprimir_datos_personales(nombre, edad, estatura)
     captured = capsys.readouterr()
-    assert captured.out == "Nombre: nombre\nEdad: edad\nEstatura: estatura\n"
+    assert captured.out == "Nombre: Tu nombre\nEdad: 30\nEstatura: 1.75\n"
